5737364a22a3e59d097ca8f72055d5de38192135
houin679 <83742598+houin679@users.noreply.github.com>
2024-11-30 17:57:54 +0800

    Update FlatMemoryAllocator.h

--- FlatMemoryAllocator.h
+++ FlatMemoryAllocator.h
@@ -2,25 +2,27 @@
 #include "IMemoryAllocator.h"
 #include <vector>
 #include <unordered_map>
+#include <memory>
 #include <iostream>
-class FlatMemoryAllocator : public IMemoryAllocator {
-public: 
-	FlatMemoryAllocator(size_t maxMemory);
-	~FlatMemoryAllocator() = default;
 
-	void* allocate(std::shared_ptr<Process> process) override;
-	void deallocate(std::shared_ptr<Process> process) override;
-	void visualizeMemory() override;
+class FlatMemoryAllocator : public IMemoryAllocator {
+public:
+    FlatMemoryAllocator(size_t maxMemory);
+    ~FlatMemoryAllocator() = default;
+
+    void* allocate(std::shared_ptr<Process> process) override;
+    void deallocate(std::shared_ptr<Process> process) override;
+    void visualizeMemory() override;
 
 private:
-	size_t maximumSize;
-	size_t allocatedSize;
-	std::vector<char> memory; //representation of memory wher 1 char = 1 byte
-	std::unordered_map<size_t, bool> allocationMap;
+    size_t maximumSize;
+    size_t allocatedSize;
+    std::vector<char> memory; // Representation of memory: 1 char = 1 byte
+    std::vector<bool> allocationMap; // Tracks allocated memory blocks
+    std::unordered_map<int, size_t> processAllocationMap; // Process ID to start index map
 
-	void initializeMemory();
-	bool canAllocateAt(size_t index, size_t size);
-	void allocateAt(size_t index, size_t size);
-	void deallocateAt(size_t index);
-
+    void initializeMemory();
+    bool canAllocateAt(size_t index, size_t size);
+    void allocateAt(size_t index, size_t size);
+    void deallocateAt(size_t index, size_t size);
 };
