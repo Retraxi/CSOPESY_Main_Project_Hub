8895b5a1ef5c14981d6ce8c3639c9599f21a7cdc
houin679 <83742598+houin679@users.noreply.github.com>
2024-12-01 01:12:45 +0800

    Create MemoryManager.h

---
+++ CSOPESY_MP/MemoryManager.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef MEMORYMANAGER_H
+#define MEMORYMANAGER_H
+
+#include <string>
+#include <unordered_map>
+#include "Process.h"
+#include <memory>
+#include "IMemoryAllocator.h"
+
+class MemoryManager {
+public:
+    MemoryManager(int maxMemory, int minPage, int maxPage);
+    ~MemoryManager() = default;
+
+    bool allocate(std::shared_ptr<Process> process);
+    void deallocate(std::shared_ptr<Process> process);
+
+    void printMem(int qq);
+    void vmstat();
+
+    IAllocator* getAllocator() {
+        return this->_allocator;
+    }
+
+private:
+    IAllocator* _allocator;
+};
+
+#endif // !MEMORYMANAGER_H
