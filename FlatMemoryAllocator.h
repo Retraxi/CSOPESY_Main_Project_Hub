c59e5aef02298c6934d9881b88f106ab2444c193
houin679 <83742598+houin679@users.noreply.github.com>
2024-11-30 18:16:10 +0800

    Update FlatMemoryAllocator.h

--- FlatMemoryAllocator.h
+++ FlatMemoryAllocator.h
@@ -7,22 +7,22 @@
 
 class FlatMemoryAllocator : public IMemoryAllocator {
 public:
-    FlatMemoryAllocator(size_t maxMemory);
-    ~FlatMemoryAllocator() = default;
+    explicit FlatMemoryAllocator(size_t maxMemory);
+    ~FlatMemoryAllocator() override = default;
 
     void* allocate(std::shared_ptr<Process> process) override;
     void deallocate(std::shared_ptr<Process> process) override;
     void visualizeMemory() override;
 
 private:
-    size_t maximumSize;
-    size_t allocatedSize;
+    size_t maximumSize; // Total memory available for allocation
+    size_t allocatedSize; // Currently allocated memory
     std::vector<char> memory; // Representation of memory: 1 char = 1 byte
-    std::vector<bool> allocationMap; // Tracks allocated memory blocks
-    std::unordered_map<int, size_t> processAllocationMap; // Process ID to start index map
+    std::vector<bool> allocationMap; // Tracks which blocks are allocated
+    std::unordered_map<int, size_t> processAllocationMap; // Maps process ID to starting index
 
-    void initializeMemory();
-    bool canAllocateAt(size_t index, size_t size);
-    void allocateAt(size_t index, size_t size);
-    void deallocateAt(size_t index, size_t size);
+    void initializeMemory(); // Initialize memory to free state
+    bool canAllocateAt(size_t index, size_t size); // Check if a block can be allocated
+    void allocateAt(size_t index, size_t size); // Mark memory as allocated
+    void deallocateAt(size_t index, size_t size); // Mark memory as free
 };
