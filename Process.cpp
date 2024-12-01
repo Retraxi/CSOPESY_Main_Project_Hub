b334d156fecccb84edc980828b47c00cf55ffcf4
houin679 <83742598+houin679@users.noreply.github.com>
2024-12-01 09:55:29 +0800

    Update Process.cpp

--- Process.cpp
+++ Process.cpp
@@ -1,12 +1,14 @@
-
 #include "Process.h"
+
 #include <fstream>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <windows.h>
+
+#include "PrintCommand.h"
 #include <mutex>
 #include <random>
-#include "PrintCommand.h"
 
 typedef std::string String;
 
@@ -15,26 +17,28 @@ int Process::sameMemory = -1;
 
 Process::Process(String name, std::uniform_int_distribution<int> commandDistr,
     std::uniform_int_distribution<int> memoryDistr,
-    std::uniform_int_distribution<int> pageDistr) : _name(std::move(name)) {
+    std::uniform_int_distribution<int> pageDistr) : _name(name) {
 
     std::lock_guard<std::mutex> lock(mtx);
-    this->_pid = Process::nextID++;
+    this->_pid = Process::nextID;
+    Process::nextID++;
     std::random_device rand_dev;
     std::mt19937 generator(rand_dev());
     int numCommands = commandDistr(generator);
-
-    // Generate a list of commands
-    for (int i = 0; i < numCommands; ++i) {
+    for (int i = 0; i < numCommands; i++) {
         this->_commandList.push_back(
             std::make_shared<PrintCommand>(
-                "Executing from " + this->_name + "!", this->_pid
+                "Hello world from " + this->_name + "!", this->_pid
             )
         );
     }
-
-    // Set required memory or use static allocation
     if (Process::sameMemory == -1) {
-        this->_requiredMemory = calculatePowerOfTwo(memoryDistr(generator));
+        this->_requiredMemory = memoryDistr(generator);
+        int power = 1;
+        while (power < this->_requiredMemory) {
+            power *= 2;
+        }
+        this->_requiredMemory = power;
     }
     else {
         this->_requiredMemory = Process::sameMemory;
@@ -44,33 +48,44 @@ Process::Process(String name, std::uniform_int_distribution<int> commandDistr,
 void Process::execute() {
     std::lock_guard<std::mutex> lock(mtx);
     if (!this->hasFinished()) {
-        this->_commandList.at(_commandCounter)->execute(
-            this->_cpuCoreID, ".\output\" + this->_name + ".txt"
-        );
-        ++this->_commandCounter;
+        this->_commandList.at(_commandCounter)->execute(this->_cpuCoreID, ".\\output\\" + this->_name + ".txt");
+        this->_commandCounter++;
     }
 }
 
-bool Process::hasFinished() const {
-    return this->_commandCounter >= this->_commandList.size();
+bool Process::hasFinished() {
+    if (this->_commandCounter >= this->_commandList.size()) {
+        return true;
+    }
+    return false;
 }
 
 int Process::setRequiredPages(int min, int max) {
     if (Process::requiredPages == -1) {
+        std::uniform_int_distribution<int>  pageDistr(min, max);
         std::random_device rand_dev;
         std::mt19937 generator(rand_dev());
-        std::uniform_int_distribution<int> pageDistr(min, max);
-        Process::requiredPages = calculatePowerOfTwo(pageDistr(generator));
+        Process::requiredPages = pageDistr(generator);
+        int power = 1;
+        while (power < Process::requiredPages) {
+            power *= 2;
+        }
+        Process::requiredPages = power;
     }
     return Process::requiredPages;
 }
 
 int Process::setRequiredMemory(int min, int max) {
     if (Process::sameMemory == -1) {
+        std::uniform_int_distribution<int>  memDistr(min, max);
         std::random_device rand_dev;
         std::mt19937 generator(rand_dev());
-        std::uniform_int_distribution<int> memDistr(min, max);
-        Process::sameMemory = calculatePowerOfTwo(memDistr(generator));
+        Process::sameMemory = memDistr(generator);
+        int power = 1;
+        while (power < Process::sameMemory) {
+            power *= 2;
+        }
+        Process::sameMemory = power;
     }
     return Process::sameMemory;
 }
@@ -78,14 +93,6 @@ int Process::setRequiredMemory(int min, int max) {
 void Process::setCPUCoreID(int cpuCoreID) {
     std::lock_guard<std::mutex> lock(mtx);
     this->_cpuCoreID = cpuCoreID;
-}
-
-int Process::calculatePowerOfTwo(int value) {
-    int power = 1;
-    while (power < value) {
-        power *= 2;
-    }
-    return power;
 }
 
 int Process::nextID = 0;
