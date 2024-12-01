8940f0ca44a697cb8b9e441e3304d7b56b3120d9
houin679 <83742598+houin679@users.noreply.github.com>
2024-12-01 12:43:51 +0800

    Update ConsoleManager.cpp

--- ConsoleManager.cpp
+++ ConsoleManager.cpp
@@ -1,90 +1,97 @@
-#include "ConsoleManager.h"
 #include "AConsole.h"
-#include "MainConsole.h"
-#include "ProcessConsole.h"
+#include "ConsoleManager.h"
 #include <iostream>
 #include <memory>
 #include <string>
+#include "MainConsole.h"
+#include "ProcessConsole.h"
+
 
 ConsoleManager* ConsoleManager::ptr = nullptr;
 
-ConsoleManager* ConsoleManager::getInstance() {
- return ConsoleManager::ptr;
+ConsoleManager* ConsoleManager::get() {
+    return ConsoleManager::ptr;
 }
 
-void ConsoleManager::initializeManager() {
+void ConsoleManager::initialize() {
     ConsoleManager::ptr = new ConsoleManager();
 }
 
-void ConsoleManager::shutdownManager() {
-      ConsoleManager::ptr = nullptr;
+void ConsoleManager::destroy() {
+    ConsoleManager::ptr = nullptr;
 }
 
 void ConsoleManager::start() {
-    this->_current->start();
+    this->_current->run();
 }
 
-bool ConsoleManager::createConsole(const std::string& name, ConsolePtr console) {
-    if (consoleRegistry.find(name) != consoleRegistry.end()) {
-        std::cerr << "Console '" << name << "' is already running!" << std::endl;
+bool ConsoleManager::newConsole(std::string name, AConsole_ console) {
+    if (this->_consoleMap.find(name) != this->_consoleMap.end()) {
+        std::cout << "Process '" + name + "' is already running!" << std::endl;
         return false;
     }
 
-    if (!console) {
-        auto processList = scheduler->getProcesses(); // Assuming `getProcesses()` exists
-        for (const auto& process : processList) {
-            if (process->getName() == name && !process->hasFinished()) {
-                console = std::make_shared<ProcessConsole>(process);
+    bool found = false;
+    if (console == nullptr) {
+        std::vector<std::shared_ptr<Process>> copyList = this->_scheduler->_processList;
+        for (int i = 0; i < copyList.size(); i++) {
+            if (name == copyList.at(i)->getName() && !copyList.at(i)->hasFinished()) {
+                console = std::make_shared<ProcessConsole>(copyList.at(i));
+                found = true;
                 break;
             }
         }
+        if (found)
+            this->_consoleMap[name] = console;
+        /*else {
+            std::uniform_int_distribution<int> commandDistr(this->_scheduler->minIns, this->_scheduler->maxIns);
+            std::uniform_int_distribution<int> memDistr(this->_scheduler->_minMemProc, this->_scheduler->_maxMemProc);
+            std::uniform_int_distribution<int> pageDistr(this->_scheduler->_minPage, this->_scheduler->_maxPage);
 
-        if (!console) {
-            std::cerr << "Unable to create console for process: " << name << std::endl;
-            return false;
-        }
+            auto newProcess = std::make_shared<Process>(name, commandDistr, memDistr, pageDistr);
+            this->_scheduler->addProcess(newProcess);
+            console = std::make_shared<ProcessConsole>(newProcess);
+            this->_consoleMap[name] = console;
+            found = true;
+        }*/
+        this->switchConsole(name);
+    }
+    else {
+        this->_consoleMap[name] = console;
     }
 
-    consoleRegistry[name] = console;
-    switchToConsole(name);
     return true;
 }
 
-void ConsoleManager::switchToConsole(const std::string& consoleName) {
-    auto it = consoleRegistry.find(consoleName);
-    if (it == consoleRegistry.end()) {
-        std::cerr << "Console '" << consoleName << "' not found." << std::endl;
+void ConsoleManager::switchConsole(std::string processName) {
+    if (this->_consoleMap.find(processName) == this->_consoleMap.end()) {
+        std::cout << "Process " + processName + " not found." << std::endl;
+        return;
+    }
+    else if (this->_consoleMap[processName]->canRemove()) {
+        this->_consoleMap.erase(processName);
+        std::cout << "Process " + processName + " not found." << std::endl;
         return;
     }
 
-    if (activeConsole) {
-        activeConsole->stop();
-    }
+    this->_current->stop();
 
-    activeConsole = it->second;
-    activeConsole->run();
+    this->_current = this->_consoleMap[processName];
+    this->_current->run();
 
-    // Wait for the console to finish its execution
-    while (activeConsole->isActive()) {}
+    // Wait for console to set active to false
+    while (this->_current->isActive()) {}
 
-    // Check if the console needs to be removed
-    if (activeConsole->canRemove()) {
-        consoleRegistry.erase(consoleName);
-        activeConsole = nullptr;
-    }
+    if (this->_current->canRemove())
+        this->_consoleMap.erase(processName);
 
-    // Return to main console
-    if (!activeConsole) {
-        activeConsole = mainConsole;
-        activeConsole->run();
-    }
+    this->_current = this->_mainConsole;
+    this->_current->run();
 }
 
 ConsoleManager::ConsoleManager() {
-    mainConsole = std::make_shared<MainConsole>(this);
-    activeConsole = mainConsole;
+    this->_mainConsole = std::make_shared<MainConsole>(MainConsole(this));
+    this->_current = this->_mainConsole;
 }
 
-ConsoleManager::~ConsoleManager() {
-   
-}
+ConsoleManager::~ConsoleManager() {}
